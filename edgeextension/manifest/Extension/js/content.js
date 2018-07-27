b3f952e4461f96271259d81641a61287f0a227dc
Hiroshi Nagata <HIROSN@LIVE.COM>
2018-07-27 00:48:35 -0700

    Reset style when inactive

--- edgeextension/manifest/Extension/js/content.js
+++ edgeextension/manifest/Extension/js/content.js
@@ -1,8 +1,9 @@
 'use strict';
 
 (function() {
-  var isActive = true,
-  updateIcon = function(active) {
+  var isActive = true;
+
+  var updateIcon = function(active) {
     if (active != isActive) {
       browser.runtime.sendMessage(active ? {
         'iconPath20': 'images/in20.png',
@@ -11,23 +12,37 @@
         'iconPath20': 'images/inactive20.png',
         'iconPath40': 'images/inactive40.png'
       });
+
       isActive = active;
     }
-  },
-  hideExpandedFooter = function() {
-    if (!isActive) { return; }
+  };
+
+  var styleExpandedFooter = function(style) {
     var expandedFooter = document.getElementById('expanded-footer');
 
     if (expandedFooter && /\/\/.+\.linkedin\.com\/feed\//.test(document.location.href)) {
-      expandedFooter.style = 'display: none;';
-    } else {
+      expandedFooter.style = style || '';
+    }
+
+    return expandedFooter && expandedFooter.style;
+  };
+
+  var hideExpandedFooter = function() {
+    if (!isActive) { return; }
+
+    if (!styleExpandedFooter('display: none;')) {
       window.requestAnimationFrame(hideExpandedFooter);
     }
-    window.setTimeout(function() { updateIcon(false); }, 1000);
+
+    window.setTimeout(function() {
+      updateIcon(false);
+      styleExpandedFooter(null);
+    }, 1000);
   };
 
   if (/\/\/.+\.linkedin\.com\//.test(document.location.href)) {
     hideExpandedFooter();
+
     window.addEventListener('scroll', function() {
       if (!isActive) {
         updateIcon(true);
